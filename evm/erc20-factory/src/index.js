72a3ef1a0f726d550e33f33f4bb0f8e67a65b3ab
Manuel Alessandro Collazo <collazo.manuel6@gmail.com>
2025-12-04 17:18:30 +0700

    chore: align repo with tutorial

--- evm/erc20-factory/src/index.js
+++ evm/erc20-factory/src/index.js
@@ -1,74 +1,86 @@
 const { createPublicClient, createWalletClient, decodeEventLog, getContract, http } = require('viem');
 const { privateKeyToAccount } = require('viem/accounts');
-const erc20Abi = require('./abis/erc20Abi.json');
-const erc20FactoryAbi = require('./abis/erc20factoryabi.json');
+const erc20Abi = require('../abis/erc20Abi.json');
+const erc20FactoryAbi = require('../abis/erc20FactoryAbi.json');
 const miniEVM = require('./chain');
 
-const privateKey = process.env.PRIVATE_KEY;
-const erc20FactoryAddress = '0xd53506E20eA25122aC6adc6462D9D1cf810Ef5a4';
+require('dotenv').config();
 
-// Create an account from the private key
+const privateKey = process.env.PRIVATE_KEY?.startsWith('0x') ? process.env.PRIVATE_KEY : `0x${process.env.PRIVATE_KEY}`;
+if (!privateKey) {
+  throw new Error('PRIVATE_KEY environment variable is not set.');
+}
+
+const erc20FactoryAddress = process.env.ERC20_FACTORY_ADDRESS;
+if (!erc20FactoryAddress) {
+  throw new Error('ERC20_FACTORY_ADDRESS environment variable is not set.');
+}
+
+// Create an account from the private key.
 const account = privateKeyToAccount(privateKey);
 
-// Create a wallet client
+// Create a wallet client.
 const client = createWalletClient({
   account,
   chain: miniEVM,
   transport: http(),
 });
 
-// create a public client
+// Create a public client.
 const publicClient = createPublicClient({
   chain: miniEVM,
   transport: http(),
 });
 
-// Send the transaction
+// Send the transaction.
 async function createERC20() {
   try {
-    // call createERC20 function on the factory contract to create a new ERC20 token
+    // Call createERC20 function on the factory contract to create a new ERC20 token.
     const hash = await client.writeContract({
-      address: erc20FactoryAddress, // Factory address
+      address: erc20FactoryAddress,
       abi: erc20FactoryAbi,
       functionName: 'createERC20',
       args: ['Test', 'TST', 18],
-    })
+    });
+
     console.log('Transaction sent. Hash:', hash);
 
-    // Wait for the transaction to be confirmed
+    // Wait briefly for the transaction to be processed.
     await new Promise(resolve => setTimeout(resolve, 500));
 
-    // Get the transaction receipt and parse the logs for the ERC20Created event
-    const receipt = await publicClient.getTransactionReceipt({
-      hash: hash
-    });
-    const erc20CreatedLog = receipt.logs.find(log => 
-      log.address.toLowerCase() === erc20FactoryAddress.toLowerCase() // Check if the log is from the factory address
+    // Get the transaction receipt and parse the logs for the ERC20Created event.
+    const receipt = await publicClient.getTransactionReceipt({ hash });
+
+    const erc20CreatedLog = receipt.logs.find(
+      // Check if the log is from the factory address.
+      log => log.address.toLowerCase() === erc20FactoryAddress.toLowerCase()
     );
 
-    // Check if the ERC20Created event was found in the logs and decode the created ERC20 address
+    // Check whether the ERC20Created event exists in the logs, then decode the created ERC20 address.
     if (erc20CreatedLog) {
       const decodedLog = decodeEventLog({
         abi: erc20FactoryAbi,
         data: erc20CreatedLog.data,
         topics: erc20CreatedLog.topics,
       });
+
       console.log('New ERC20 address:', decodedLog.args.erc20);
 
-      // Try reading the new ERC20 contract
+      // Try reading data from the new ERC20 contract.
       const erc20 = await getContract({
         address: decodedLog.args.erc20,
         abi: erc20Abi,
         client: {
           public: publicClient,
-          wallet: client
-        }
+          wallet: client,
+        },
       });
+
       console.log('ERC20 name:', await erc20.read.name());
       console.log('ERC20 symbol:', await erc20.read.symbol());
       console.log('ERC20 decimals:', await erc20.read.decimals());
     } else {
-      console.log('ERC20Created event not found in the logs');
+      console.log('ERC20Created event not found in logs');
     }
 
   } catch (error) {
